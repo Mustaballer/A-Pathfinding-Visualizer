55b763e4edf0d6ac6b6656c47a2c9734db4928d9
Mustafa Abdulrahman <1abdulrahmus@hdsb.ca>
2020-05-01 20:53:01 -0400

    import p5.js

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var cols = 5;
+var rows = 5;
+var grid = new Array(cols);
+
+function Spot() {
+    this.f = 0;
+    this.g = 0;
+    this.h = 0;
+}
+
+function setup() {
+    createCanvas(400, 400);
+    console.log('A*');
+
+    // Making a 2D array
+    for (let i = 0; i < cols; i++) {
+        grid[i] = new Array(rows);
+    }
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = new Spot();
+        }
+    }
+
+    console.log(grid)
+}
+
+
+function draw() {
+    background(0);
+
+
+}
+
+
+
